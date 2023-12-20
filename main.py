b0287f2491e6d2a35250486b3a26ae350e190abf
Lucas Harã <lucashara20@gmail.com>
2023-12-20 18:19:15 -0300

    Alterado corpo e anexo do email

--- main.py
+++ main.py
@@ -39,30 +39,61 @@ def executar_consulta_sql(arquivo_sql):
     return result
 
 
+def auto_ajustar_colunas(worksheet):
+    for col in worksheet.columns:
+        max_length = 0
+        column = col[0].column_letter  # Get the column name
+        for cell in col:
+            try:  # Necessary to avoid error on empty cells
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2)
+        worksheet.column_dimensions[column].width = adjusted_width
+
 def gerar_excel_em_memoria(dados_diarios, dados_mes):
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
         # Dados diários
         nome_aba_diaria = datetime.now().strftime('%d %m %Y')
         dados_diarios.to_excel(writer, sheet_name=nome_aba_diaria, index=False)
+        wb = writer.book
+        auto_ajustar_colunas(wb[nome_aba_diaria])
 
         # Dados mensais
         nome_aba_mensal = datetime.now().strftime('%B %Y')
         dados_mes.to_excel(writer, sheet_name=nome_aba_mensal, index=False)
+        auto_ajustar_colunas(wb[nome_aba_mensal])
     
     output.seek(0)
     return output
 
 def construir_corpo_email(dados):
-    corpo_email = "<html><body><p>Prezados,</p><p>Segue o relatório referente às alterações de preços detectadas:</p>"
-    corpo_email += "<h2>Empresa</h2><ul style='list-style-type:none;'>"
-    for filial, quantidade in dados.groupby('codfilial').size().items():
+    # Convertendo os nomes das colunas para minúsculas
+    dados.columns = [col.lower() for col in dados.columns]
+
+    corpo_email = "<html><body><p>Prezados,</p><p>Segue o relatório referente a corte e falta de itens:</p>"
+    corpo_email += "<h2>Resumo por Filial</h2>"
+
+    # Certifique-se de que a coluna 'codfilial' está em formato string
+    dados['codfilial'] = dados['codfilial'].astype(str)
+
+    # Agrupando dados por 'codfilial'
+    agrupamento_filial = dados.groupby('codfilial')
+
+    # Calculando a soma para cada filial
+    sumario_filial = agrupamento_filial[['qt_falta', 'qt_corte', 'pvenda_falta', 'pvenda_corte']].sum()
+
+    for filial, sumario in sumario_filial.iterrows():
         nome_empresa = "Farmaum PB" if filial == '1' else "Farmaum RN" if filial == '2' else "Brasil" if filial == '3' else "Outra"
-        corpo_email += f"<li><strong>{nome_empresa}</strong>: {quantidade}</li>"
-    corpo_email += "</ul><h2>Comprador</h2><ul style='list-style-type:none;'>"
-    for comprador, alteracoes in dados.groupby('comprador')['codprod'].nunique().items():
-        corpo_email += f"<li><strong>{comprador}</strong>: {alteracoes}</li>"
-    corpo_email += "</ul></body></html>"
+        corpo_email += f"<h3>{nome_empresa}</h3>"
+        corpo_email += f"<p>Quantidade Total de Faltas: {int(sumario['qt_falta'])}</p>"
+        corpo_email += f"<p>Quantidade Total de Cortes: {int(sumario['qt_corte'])}</p>"
+        corpo_email += f"<p>Valor Total de Faltas: R$ {sumario['pvenda_falta']:,.2f}</p>"
+        corpo_email += f"<p>Valor Total de Cortes: R$ {sumario['pvenda_corte']:,.2f}</p>"
+
+    corpo_email += "</body></html>"
     return corpo_email
 
 
@@ -77,7 +108,7 @@ def enviar_email(assunto, corpo, excel_data):
 
         msg.attach(MIMEText(corpo, 'html'))
 
-        nome_arquivo_excel = f"Sentinela Preço {datetime.now().strftime('%d %m %Y')}.xlsx"
+        nome_arquivo_excel = f"Relatório de Corte e Falta {datetime.now().strftime('%d %m %Y')}.xlsx"
         part = MIMEApplication(excel_data.read())
         part.add_header('Content-Disposition', 'attachment', filename=nome_arquivo_excel)
         msg.attach(part)
@@ -92,25 +123,25 @@ def enviar_email(assunto, corpo, excel_data):
 
 
 def verificar_mudancas():
-    if datetime.now().hour == 17 and datetime.now().minute == 50:
-        logging.info("Iniciando a verificação de mudanças de preço.")
+    if datetime.now().hour == 8 and datetime.now().minute == 0:
+        logging.info("Iniciando a verificação de corte e falta de itens.")
         try:
             dados_diarios = executar_consulta_sql('sintetico_corte_falta.sql')
             dados_mes = executar_consulta_sql('analitico_corte_falta.sql')
 
             total_alteracoes = dados_diarios['codprod'].nunique()
-            logging.info(f"Total de alterações detectadas: {total_alteracoes}")
+            logging.info(f"Total de itens com corte e falta detectados: {total_alteracoes}")
 
             if total_alteracoes > 0:
                 excel_data = gerar_excel_em_memoria(dados_diarios, dados_mes)
                 corpo_email = construir_corpo_email(dados_diarios)
-                assunto_email = f"Sentinela de preços - Tivemos {total_alteracoes} alterações de preços {datetime.now().strftime('%d/%m/%Y')}"
+                assunto_email = f"Relatório de Corte e Falta de Itens - {datetime.now().strftime('%d/%m/%Y')}"
                 enviar_email(assunto_email, corpo_email, excel_data)
                 logging.info("Email enviado com sucesso!")
             else:
-                logging.info("Nenhuma alteração detectada, e-mail não enviado.")
+                logging.info("Nenhum item com corte ou falta detectado, e-mail não enviado.")
 
-            logging.info("Verificação de mudanças de preço concluída com sucesso.")
+            logging.info("Verificação de corte e falta de itens concluída com sucesso.")
         except Exception as e:
             logging.error(f"Erro ao processar os dados: {e}")
 
